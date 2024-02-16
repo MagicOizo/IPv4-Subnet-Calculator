bb65b71c5aa3fb9bc6a0df280a1fb86a7dcc2ea4
Max Zöller <MagicOizo@users.noreply.github.com>
2024-02-16 09:59:47 +0100

    Added missing files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace IPv4SubnetCalculator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new mainForm());
+        }
+    }
+}
